fc79c12f728e004f7e83bf571533fb157c682fc6
beefr <coreno@orange.fr>
2023-08-16 18:54:11 +0100

    adapting voice

--- mainModule/voiceOutput.py
+++ mainModule/voiceOutput.py
@@ -5,7 +5,8 @@ class VoiceOutput(object):
     def __init__(self):
         self._engine= pyttsx3.init()
         self._engine.setProperty('voice', 'french')
-
+        self._engine.setProperty('rate',105)
+        self._engine.setProperty('pitch', 0.8)
 
     def translate(self, text):
         self._engine.say(text)
