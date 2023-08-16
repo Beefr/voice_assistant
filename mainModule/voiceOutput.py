63e775e37e83bab9938de281ac43dfb490978f55
beefr <coreno@orange.fr>
2023-08-16 09:13:59 +0100

    removed playsound

--- mainModule/voiceOutput.py
+++ mainModule/voiceOutput.py
@@ -1,5 +1,4 @@
 from gtts import gTTS
-#from playsound import playsound
 import os
 
 class VoiceOutput(object):
@@ -14,7 +13,6 @@ class VoiceOutput(object):
     def translate(self, text):
         speech = gTTS(text, lang='fr')
         speech.save('./output.mp3')
-        #playsound('./output.mp3')
         os.system('mpg123 -q ./output.mp3')
         os.remove('./output.mp3')
         return
