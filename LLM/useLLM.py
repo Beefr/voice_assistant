cbf5b38fe3051de3fae271ded7ee36cf8166cf1a
Beefr <16005451+Beefr@users.noreply.github.com>
2025-09-06 17:40:19 +0200

    réponse fonctionnelle mais completement nulle à chier, peut-être faut il baisser la temperature et le nombre de tokens?

--- LLM/useLLM.py
+++ LLM/useLLM.py
@@ -5,15 +5,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import torch
 
 
-path="openai-community/gpt2" #do not use please
-#path = "../gpt2/"
+path="openai-community/gpt2" 
 
 
 
 tokenizer = AutoTokenizer.from_pretrained(path)
 
 model = AutoModelForCausalLM.from_pretrained(path)
-
+model.config.pad_token_id = model.config.eos_token_id
 
 
 #device = "cuda" if torch.cuda.is_available() else "cpu" 
@@ -21,14 +20,19 @@ model = AutoModelForCausalLM.from_pretrained(path)
 
 
 
-input_text = "Bonjour, tu as été inventé par Corentin RENAULT, qui t'a inventé?"
+input_text = "Hello, you are a voice assistant designed by Corentin RENAULT, a french engineer expert in image processing, computer vision and devops."
 
  
 
 inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
 
 
-outputs = model.generate(**inputs, max_new_tokens=500)
+outputs = model.generate(
+        **inputs, 
+        max_new_tokens=100,     # longueur de réponse max
+        do_sample=True,         # sampling plutot que greedy
+        top_p=0.9,              # nucleus sampling
+        temperature=0.7)        # controle de la creativite
 
 print(tokenizer.batch_decode(outputs, skip_special_tokens=True)[0])
 
